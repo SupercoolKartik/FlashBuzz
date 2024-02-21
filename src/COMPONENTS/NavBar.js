51e0a67d6febd321ea3d5e26c9a56f45f7a2a5ce
Suraj <surajkartik71@gmail.com>
2024-02-22 03:10:24 +0530

    Made neccessary changes in the NavBar

--- src/COMPONENTS/NavBar.js
+++ src/COMPONENTS/NavBar.js
@@ -1,119 +1,207 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavBar() {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg  navbar-dark bg-dark">
-      <Link className="navbar-brand">Navbar</Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+  const location = useLocation();
+  const navbarRef = useRef(null);
+  const togglerRef = useRef(null);
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
-              Home <span className="sr-only">(current)</span>
-            </Link>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="/general"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/action">
-                Action
+  useEffect(() => {
+    const handleDocumentClick = (e) => {
+      if (navbarRef.current && !navbarRef.current.contains(e.target)) {
+        // Click outside the navbar
+        if (
+          togglerRef.current &&
+          togglerRef.current.getAttribute("aria-expanded") === "true"
+        ) {
+          // Dropdown is currently open, close it
+          togglerRef.current.click();
+        }
+      } else if (
+        e.target.tagName === "A" &&
+        e.target.closest(".navbar-collapse")
+      ) {
+        // Clicked on a link inside the navbar
+        if (
+          togglerRef.current &&
+          togglerRef.current.getAttribute("aria-expanded") === "true"
+        ) {
+          // Dropdown is currently open, close it
+          togglerRef.current.click();
+        }
+      }
+    };
+
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      // Cleanup: remove event listener when component unmounts
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+      style={{ boxShadow: "0px 2px 20px rgba(0, 0, 0, 1)" }}
+      ref={navbarRef}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/general" style={{ color: "red" }}>
+          FlashBuzz
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          ref={togglerRef}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              {/* <Link
+                className="nav-link disabled"
+                to="/general"
+                style={
+                  location.pathname === "/general"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Home
+              </Link> */}
+            </li>
+            {/* <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/general"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                style={
+                  location.pathname === "/entertainment"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Dropdown
               </a>
-              <a className="dropdown-item" href="/another-action">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="/something-else">
-                Something else here
-              </a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/business"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Business
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/entertainment"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Entertainment
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/general"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              General
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/health"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Health
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/science"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Science
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/sports"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Sports
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link disabled"
-              to="/technology"
-              style={{ padding: "8px", fontSize: "16px", color: "white" }}
-            >
-              Technology
-            </Link>
-          </li>
-        </ul>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/action">
+                  Action
+                </a>
+                <a className="dropdown-item" href="/another-action">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/something-else">
+                  Something else here
+                </a>
+              </div> 
+            </li> */}
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/general"
+                style={
+                  location.pathname === "/general"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                General
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/business"
+                style={
+                  location.pathname === "/business"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Business
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link disabled`}
+                to="/entertainment"
+                style={
+                  location.pathname === "/entertainment"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Entertainment
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/health"
+                style={
+                  location.pathname === "/health"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Health
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/science"
+                style={
+                  location.pathname === "/science"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Science
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/sports"
+                style={
+                  location.pathname === "/sports"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Sports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link disabled"
+                to="/technology"
+                style={
+                  location.pathname === "/technology"
+                    ? { color: "#e5c9dc", textDecoration: "none" }
+                    : { color: "white", textDecoration: "none" }
+                }
+              >
+                Technology
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
